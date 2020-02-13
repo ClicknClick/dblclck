bb673a3c7634023e06fd4bca1299a1b30c8f8c17
Grig Alex <alexengrigdev@gmail.com>
2020-02-13 18:47:02 +0300

    Initial commit :tada:

---
+++ src/main/java/dev/alexengrig/dblclck/DblclckApplication.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.dblclck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DblclckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DblclckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/alexengrig/dblclck/DblclckApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.dblclck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DblclckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
